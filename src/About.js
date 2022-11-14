935d176f748575b92d4435c71d8a5c28ca94858e
torontopubliclibra <dana.r.teagle@gmail.com>
2022-11-14 16:33:01 -0500

    updated About text

--- src/About.js
+++ src/About.js
@@ -3,9 +3,9 @@ const About = () => {
         <main>
             <h2>About Trans I.D.</h2>
             <p>The Trans I.D. guide is an app for individuals going through the process of changing their name and/or gender marker with governmental bodies in Canada.</p>
-            <p>The process of updating your information can be confusing and inaccessible. The Trans I.D. guide is designed for anyone navigating this path—whether you are trans, Two-Spirit, non-binary, or gender diverse—because every one of us deserves to have our documents accurately reflect our identities.</p>
-            <blockquote>Just like your identity, your path to updating your I.D. will be unique to you. This guide is a user-friendly resource that tailors itself to each person's context and experience.</blockquote>
-            <p>This app is developed and maintained by <a href="https://danateagle.com" target="_blank" rel="noreferrer">Dana Teagle</a>, a non-binary trans woman and web developer, who previously offered this service through one-on-one consultations. <a href="mailto:dana.r.teagle@gmail.com" target="_blank" rel="noreferrer">Get in touch</a> to book Dana for a Trans I.D. workshop.</p>
+            <p>The process of updating your information can be confusing and inaccessible. This app is designed for anyone navigating this path—whether you are trans, Two-Spirit, non-binary, or gender diverse—because every one of us deserves to have our documents accurately reflect our identities.</p>
+            <blockquote>Just like your identity, your path to updating your I.D. will be unique to you. The Trans I.D. guide is a user-friendly resource that tailors itself to each person's context and experience.</blockquote>
+            <p>Developed and maintained by <a href="https://danateagle.com" target="_blank" rel="noreferrer">Dana Teagle</a>, a non-binary trans woman and web developer, this service was previously offered through one-on-one consultations. <a href="mailto:dana.r.teagle@gmail.com" target="_blank" rel="noreferrer">Get in touch</a> to book Dana for a Trans I.D. workshop.</p>
         </main>
     )
 }

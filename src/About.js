7cab52940ddac5bca0c9fcb1d7ce1e3a2524c871
torontopubliclibra <dana.r.teagle@gmail.com>
2022-11-14 08:07:25 -0500

    updated mobile styles and added links page

--- src/About.js
+++ src/About.js
@@ -1,10 +1,12 @@
+import { Link } from "react-router-dom";
+
 const About = () => {
     return (
         <main>
             <h2>About Trans I.D.</h2>
             <p>The Trans I.D. app is a guide for individuals going through the process of changing their name and/or gender marker with the governmental bodies of Canada, and it is intended for use by any and all individuals, be they Trans, Two-Spirit, non-binary, gender diverse, or otherwise in need.</p>
             <blockquote>Each and every one of us deserves to have our documents properly represent our identity and personhood.</blockquote>
-            <p>This service is designed with the belief that there is no average experience for any person who may be seeking these changes, and the understanding that navigating governmental bureaucracy can be incredibly daunting and anxiety-inducing. It is developed and maintained by <a href="https://danateagle.com" target="_blank" rel="noreferrer">Dana Teagle</a>, a non-binary trans woman and web developer, who previously offered this service through one-on-one consultations. To enquire about booking Dana for Trans I.D. workshops, <a href="mailto:dana.r.teagle@gmail.com">click here</a>.</p>
+            <p>This service is designed with the belief that there is no average experience for anyone who may seek these changes, and the understanding that navigating governmental bureaucracy can be incredibly daunting and inaccessible. It is developed and maintained by <a href="https://danateagle.com" target="_blank" rel="noreferrer">Dana Teagle</a>, a non-binary trans woman and web developer, who previously offered this service through one-on-one consultations. <a href="mailto:dana.r.teagle@gmail.com" target="_blank" rel="noreferrer">Get in touch</a> to book Dana for a Trans I.D. workshop.</p>
         </main>
     )
 }
--- src/Guide.js
+++ src/Guide.js
@@ -1,20 +1,27 @@
-const Main = () => {
+import { useState } from "react";
+
+const Guide = () => {
+
+    const [ emailInput, setEmailInput ] = useState("");
 
     const handleSubscribe = (event) => {
+
         event.preventDefault();
+
+        setEmailInput("");
     }
 
     return (
         <main>
-            <h2>This app is currently in development.</h2>
+            <h2>The guide is currently in development.</h2>
             <p>Sign up below to be emailed when it launches.</p>
             <form onSubmit={handleSubscribe}>
                 <label htmlFor="email">Email:</label>
-                <input id="email" type="email"></input>
+                <input id="email" type="email" value={emailInput} onInput={(event) => {setEmailInput(event.target.value)}}></input>
                 <button type="submit">Subscribe</button>
             </form>
         </main>
     )
 }
 
-export default Main;
+export default Guide;

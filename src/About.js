c16c970d2cf3a66294d57044034e30c207a85514
torontopubliclibra <dana.r.teagle@gmail.com>
2022-11-14 15:37:24 -0500

    updated Ontario links and About text

--- src/About.js
+++ src/About.js
@@ -2,9 +2,10 @@ const About = () => {
     return (
         <main>
             <h2>About Trans I.D.</h2>
-            <p>The Trans I.D. guide is an app for individuals going through the process of changing their name and/or gender marker with the governmental bodies of Canada, and it is intended for use by any and all individuals, be they Trans, Two-Spirit, non-binary, gender diverse, or otherwise in need.</p>
-            <blockquote>Each and every one of us deserves to have our documents properly represent our identity and personhood.</blockquote>
-            <p>This service is designed with the belief that there is no average experience for anyone who may seek these changes, and the understanding that navigating governmental bureaucracy can be incredibly daunting and inaccessible. It is developed and maintained by <a href="https://danateagle.com" target="_blank" rel="noreferrer">Dana Teagle</a>, a non-binary trans woman and web developer, who previously offered this service through one-on-one consultations. <a href="mailto:dana.r.teagle@gmail.com" target="_blank" rel="noreferrer">Get in touch</a> to book Dana for a Trans I.D. workshop.</p>
+            <p>The Trans I.D. guide is an app for individuals going through the process of changing their name and/or gender marker with governmental bodies in Canada.</p>
+            <p>The process of updating your information can be confusing and inaccessible. The Trans I.D. guide is designed for anyone navigating this path—whether you are trans, Two-Spirit, non-binary, or gender diverse—because every one of us deserves to have our documents accurately reflect our identities.</p>
+            <blockquote>Just like your identity, your path to updating your I.D. will be unique to you. This guide is a user-friendly resource that tailors itself to each person's context and experience.</blockquote>
+            <p>This app is developed and maintained by <a href="https://danateagle.com" target="_blank" rel="noreferrer">Dana Teagle</a>, a non-binary trans woman and web developer, who previously offered this service through one-on-one consultations. <a href="mailto:dana.r.teagle@gmail.com" target="_blank" rel="noreferrer">Get in touch</a> to book Dana for a Trans I.D. workshop.</p>
         </main>
     )
 }
--- src/Links.js
+++ src/Links.js
@@ -8,28 +8,28 @@ const Links = () => {
         <div className="linksAndResources">
             <h3>Name Changes</h3>
             <ul>
-                <li><a href="https://www.ontario.ca/page/change-name" target="_blank" rel="noreferrer">Ontario Name Change Info</a></li>
-                <li><a href="http://www.forms.ssb.gov.on.ca/mbs/ssb/forms/ssbforms.nsf/GetFileAttach/007-11155E~3/$File/11155E.pdf" target="_blank" rel="noreferrer">Ontario Name Change Application</a></li>
+                <li><a href="https://www.ontario.ca/page/change-name" target="_blank" rel="noreferrer">Name Change Information</a></li>
+                <li><a href="http://www.forms.ssb.gov.on.ca/mbs/ssb/forms/ssbforms.nsf/GetFileAttach/007-11155E~3/$File/11155E.pdf" target="_blank" rel="noreferrer">Name Change Application</a></li>
                 <li><a href="http://www.forms.ssb.gov.on.ca/mbs/ssb/forms/ssbforms.nsf/GetFileAttach/007-11320E~1/$File/11320E.pdf" target="_blank" rel="noreferrer">Request for Non-Publication in Ontario Gazette</a></li>
             </ul>
             <h3>Health Card Updates</h3>
             <ul>
-                <li><a href="https://www.ontario.ca/page/replace-cancel-or-change-information-your-health-card" target="_blank" rel="noreferrer">Ontario Health Card Name Update Info</a></li>
-                <li><a href="https://forms.mgcs.gov.on.ca/en/dataset/7918268c-820f-4bb7-99e6-a9f187745cd0/resource/1944dd02-6645-4915-a978-60d2dc652fed/download/0280-82e.pdf" target="_blank" rel="noreferrer">Ontario Health Card Name Update Application</a></li>
+                <li><a href="https://www.ontario.ca/page/replace-cancel-or-change-information-your-health-card" target="_blank" rel="noreferrer">Health Card Update Information</a></li>
+                <li><a href="https://forms.mgcs.gov.on.ca/en/dataset/7918268c-820f-4bb7-99e6-a9f187745cd0/resource/1944dd02-6645-4915-a978-60d2dc652fed/download/0280-82e.pdf" target="_blank" rel="noreferrer">Health Card Update Application</a></li>
             </ul>
             <h3>Gender Marker Changes</h3>
             <ul>
-                <li><a href="https://www.ontario.ca/page/changing-your-sex-designation-your-birth-registration-and-birth-certificate" target="_blank" rel="noreferrer">Ontario Birth Certificate Sex Designation Change Info</a></li>
-                <li><a href="file:///Users/danateagle/Downloads/11331E.pdf" target="_blank" rel="noreferrer">Sex Designation Change Application</a></li>
+                <li><a href="https://www.ontario.ca/page/changing-your-sex-designation-your-birth-registration-and-birth-certificate" target="_blank" rel="noreferrer">Birth Certificate Sex Designation Change Info</a></li>
+                <li><a href="file:///Users/danateagle/Downloads/11331E.pdf" target="_blank" rel="noreferrer">Birth Certificate Sex Designation Change Application</a></li>
             </ul>
             <h3>Passport Updates</h3>
             <ul>
-                <li><a href="https://www.canada.ca/en/immigration-refugees-citizenship/services/canadian-passports/change-sex.html" target="_blank" rel="noreferrer">Canada Passport Gender Marker Update Info</a></li>
-                <li><a href="https://www.canada.ca/content/dam/ircc/migration/ircc/english/passport/forms/pdf/pptc643-eng.pdf" target="_blank" rel="noreferrer">Canada Passport Gender Marker Update Application</a></li>
+                <li><a href="https://www.canada.ca/en/immigration-refugees-citizenship/services/canadian-passports/change-sex.html" target="_blank" rel="noreferrer">Passport Gender Marker Update Info</a></li>
+                <li><a href="https://www.canada.ca/content/dam/ircc/migration/ircc/english/passport/forms/pdf/pptc643-eng.pdf" target="_blank" rel="noreferrer">Passport Gender Marker Update Application</a></li>
             </ul>
             <h3>Canadian SIN Updates</h3>
             <ul>
-                <li><a href="https://catalogue.servicecanada.gc.ca/content/EForms/en/CallForm.html?Lang=en&PDF=GC-NAS2120.pdf" target="_blank" rel="noreferrer">Social Insurance Number Information Change Application</a></li>
+                <li><a href="https://catalogue.servicecanada.gc.ca/content/EForms/en/CallForm.html?Lang=en&PDF=GC-NAS2120.pdf" target="_blank" rel="noreferrer">Social Insurance Number Update Application</a></li>
             </ul>
         </div>
     )

1cfef8a25ea0d73c9db8d9a9b9a46dddfd414342
torontopubliclibra <dana.r.teagle@gmail.com>
2022-11-18 07:22:18 -0500

    began working out ontario functionality

--- src/Guide.js
+++ src/Guide.js
@@ -1,36 +1,53 @@
 import { useState } from "react";
 
-// import firebase functions
-import firebase from './firebase';
-import { getDatabase, ref, push } from 'firebase/database'
+import Ontario from "./Ontario";
+
+// // import firebase functions
+// import firebase from './firebase';
+// import { getDatabase, ref, push } from 'firebase/database'
 
 const Guide = () => {
 
-    const [ emailInput, setEmailInput ] = useState("");
+    // const [ emailInput, setEmailInput ] = useState("");
+    const [ province, setProvince ] = useState("");
+    // const [ age, setAge ] = useState("");
+    // const [ residency, setResidency ] = useState("");
 
-    // set firebase endpoint
-    const database = getDatabase(firebase)
-    const databaseRef = ref(database, `/emails`)
+    // // set firebase endpoint
+    // const database = getDatabase(firebase)
+    // const databaseRef = ref(database, `/emails`)
 
-    const handleSubscribe = (event) => {
+    // const handleSubscribe = (event) => {
 
-        event.preventDefault();
+    //     event.preventDefault();
 
-        // push the item to the firebase endpoint
-        push(databaseRef, emailInput);
+    //     // push the item to the firebase endpoint
+    //     push(databaseRef, emailInput);
 
-        setEmailInput("");
-    }
+    //     setEmailInput("");
+    // }
+
+    const ProvinceSelect = (
+            <>
+            <div className="provinceSelect">
+                <label htmlFor="provinces">Which province do you live in?</label>
+                <select name="provinces" id="provinces" defaultValue="" onChange={(event) => {setProvince(event.target.value)}}>
+                    <option value="">Select a province</option>
+                    <option value="Ontario">Ontario</option>
+                </select>
+            </div>
+            </>
+        )
 
     return (
         <main>
             <h2>Trans I.D. Guide</h2>
-            <p>The guide is currently in development. Sign up below to be emailed when it launches.</p>
-            <form onSubmit={handleSubscribe}>
-                <label htmlFor="email">Email:</label>
-                <input id="email" type="email" value={emailInput} onInput={(event) => {setEmailInput(event.target.value)}}></input>
-                <button type="submit">Subscribe</button>
-            </form>
+            {ProvinceSelect}
+            {
+                province === "Ontario"
+                ? <Ontario />
+                : null
+            }
         </main>
     )
 }
